4a60c3a89c68c095645cad61cbe1bfbe2b38047f
sufyan <stihami11@gmail.com>
2017-03-08 23:26:19 -0800

    Create HashTable.h

---
+++ HashTable.h
@@ -0,0 +1,16 @@
+#ifndef _HASHTABLE_H_
+#def _HASHTABLE_H_
+
+class HashTable {
+private:
+
+public:
+HashTable(int size) {
+ int array[] = new int[size];
+ }
+ ~HashTable() {
+ delete [] array;
+ }
+};
+
+#endif
