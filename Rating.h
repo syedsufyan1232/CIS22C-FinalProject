c0b23efffd0ced60f3b9e4206945ba890f6c76fa
sufyan <stihami11@gmail.com>
2017-03-08 23:29:27 -0800

    Update Rating.h

--- Rating.h
+++ Rating.h
@@ -1,12 +1,18 @@
 #ifndef _RATING_H_
 #def _RATING_H_
 
+#include <string>
+#include "LinkedList.h"
+
+using namespace std;
+
 class Rating {
 private:
-int score;
-int difficulty;
-string comments;
+  int score;
+  int difficulty;
+  LinkedList<string> comments;
 public:
 
 };
+
 #endif
