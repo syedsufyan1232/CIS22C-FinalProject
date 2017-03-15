f5802c3013a90e9c23472f3d3d7bd4da62d9ad21
ericchung93 <ericchung93@gmail.com>
2017-03-15 16:52:54 -0700

    Update Professor.h

--- Professor.h
+++ Professor.h
@@ -1,23 +1,48 @@
-#ifndef _Professor_H_
-#def _Professor_H_
+#ifndef PROFESSOR_H_
+#define PROFESSOR_H_
 
-#include <string>
+#include <iostream>
 #include "Rating.h"
-
+#include <string>
 using namespace std;
 
-class Professor {
+class Professor
+{
 private:
-Rating ratings;
-double avScore;
-double avDifficulty;
-
+	string field;
+	string firstName;
+	string lastName;
+	double avgScore;
+	double avgDiff;
+	Rating rating;
+	int ratingSize;
 public:
-Professor() {}
-//Professor(a, b, c) {} For example
-Professor(Professor &obj) {}
-~Professor() {}
+
+	Professor();
+	~Professor();
+
+	//accessors
+	void setField(string f) { field = f; }
+	void setfirstName(string fn) { firstName = fn; }
+	void setlastName(string ln) { lastName = ln; }
+	void setavgScore(double as) { avgScore = as;}
+	void setavgDiff(double ad) { avgDiff = ad; }
+	void setratingSize(int rs) { ratingSize = rs; }
+
+	//mutators
+	string getField() { return field; }
+	string getfirstName() { return firstName; }
+	string getlastName() { return lastName; }
+	double getavgScore() { return avgScore; }
+	double getavgDiff() { return avgDiff; }
+	int getratingSize() { return ratingSize; }
+
+	//behaviors
+	string blockstring();
+	string smallstring();
+	bool operator< (const Professor& p);
+	friend istream& operator>> (istream& i, Professor& p);
+	friend ostream& operator<< (ostream& o, const Professor& p);
 
 };
-
 #endif
